53924c82997db8147353d186794001f261973b3f
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-04-09 00:36:59 +0330

    tamrin datatype in python

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+# dar in file tamam nokat python fandamentl hast bahamin tor pishrafteh
 print("hi i am ehsan")
+
+# data types
+# string
+print(type("ehsan"))
+# number 
+    # int
+print(type(180))
+    # float
+print(type(180.8888))
+    # complex
+print(type(180j))
