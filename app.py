aac0effb4e7c7c8f7b36d236e15cf16c289adf84
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-04-11 00:46:02 +0330

    variables

--- app.py
+++ app.py
@@ -20,3 +20,12 @@ print(type(180j))
 # %
 # ** => tavan
 # // => haman taghsim ast vali int
+
+# variable
+# baraieh create iek variable dar python beh shekleh zir amal mikonim 
+# baraieh sakhteh subet ham name on baiad ba horoofeh bozorg bashad
+# nemitavan bein name ha faseleh gozasht va inkeh ba number va namad shoroonemishavad
+
+name = "ehsan"
+age = 19
+SUBET = "ehsan" # in sabet nemishavad valieh behtar ast name on insheklii bashad
