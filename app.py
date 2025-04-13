11befab5ff6a6621e61817056b3f9fc662b3b5bc
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-04-14 00:24:56 +0330

    casting

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 # dar in file tamam nokat python fandamentl hast bahamin tor pishrafteh
-print("hi i am ehsan")
+# print("hi i am ehsan")
 
 # data types
 # string
-print(type("ehsan"))
+# print(type("ehsan"))
 # number 
     # int
-print(type(180))
+# print(type(180))
     # float
-print(type(180.8888))
+# print(type(180.8888))
     # complex
-print(type(180j))
+# print(type(180j))
 
 # operator
 # +
@@ -26,6 +26,18 @@ print(type(180j))
 # baraieh sakhteh subet ham name on baiad ba horoofeh bozorg bashad
 # nemitavan bein name ha faseleh gozasht va inkeh ba number va namad shoroonemishavad
 
-name = "ehsan"
-age = 19
-SUBET = "ehsan" # in sabet nemishavad valieh behtar ast name on insheklii bashad
+# name = "ehsan"
+# age = 19
+# SUBET = "ehsan" # in sabet nemishavad valieh behtar ast name on insheklii bashad
+
+# type casting in python
+
+# float string ra nemitavan keh beh int tarig kard va inkeh esm ia character ra imitavan adad kard
+# mored badi mesl js nist keh string ra dar kenar number bogzarad kolan error midahad
+
+# x = int(3.2)
+# x2 = int("83342")
+# y = float(3)
+# y2 = float("5")
+# z = str(3.6)
+# print(x , x2 , y , y2 , z)
