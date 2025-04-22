68c05d6ed12c47a9a8008ec111ed770a752e8f49
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-04-23 00:18:54 +0330

    finish task first project

---
+++ projects/one.py
@@ -0,0 +1,12 @@
+# num = int(input("please enter a numbert between 1-9 :"))
+# count = num
+
+# count *= 2
+# count += 8
+# count += num
+# count -= 2
+# count /= 3
+# count -= num
+# count *= 4
+
+# print(count)
