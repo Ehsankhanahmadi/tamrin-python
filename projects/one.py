06d3a8ff8a2b48aad357771c01813f5473025b51
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-04-30 17:43:22 +0330

    remove comment

--- projects/one.py
+++ projects/one.py
@@ -1,12 +1,12 @@
-# num = int(input("please enter a numbert between 1-9 :"))
-# count = num
+num = int(input("please enter a numbert between 1-9 :"))
+count = num
 
-# count *= 2
-# count += 8
-# count += num
-# count -= 2
-# count /= 3
-# count -= num
-# count *= 4
+count *= 2
+count += 8
+count += num
+count -= 2
+count /= 3
+count -= num
+count *= 4
 
-# print(count)
+print(count)
