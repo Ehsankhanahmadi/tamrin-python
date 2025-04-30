7772559d4434f9d605e23a1b3a99d0c7addf6122
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-04-30 13:42:46 +0330

    tamrin ask

--- projects/two.py
+++ projects/two.py
@@ -1,19 +1,29 @@
-num = int(input("please enter a number :"))
-sum = 0
+num = int(input("please enter a number two : "))
+num1 = int(input("please enter a number one  : "))
+num2 = num
+num3 = num
+sum = 1
+array = []
+if(num == 1):
+    print(sum)
+else:
+    while(num2 > num1):
+        num2 -= 1
+        sum = 1
+        while(num > 1):
+            if(num % 2 == 0):
+                num /= 2
+                sum += 1
+            else:
+                num = num*3 + 1
+                sum += 1
+        num = num2
+        array.append(sum)
 
-while(num > 1):
-    if(num % 2 == 0):
-        num /= 2
-        sum += 1
-    else:
-        num = num*3 + 1
-        sum += 1
+array.sort(reverse=True)
+print(num1 , num3 , array[0])
 
-print(sum)
-
-# 10 / 2 = 5
-# 5 * 3 + 1 = 16
-# 16 / 2 = 8 
-# 8 / 2 = 4
-# 4 / 2 = 2
-# 2 / 2 = 1
+# 1 10 20
+# 100 200 125
+# 201 210 89
+# 900 1000 174
